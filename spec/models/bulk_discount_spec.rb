adcf2a271bbec52d31ff0e88bb59417b6a6d05f4
Christian Valesares <cvalesares@gmail.com>
2021-11-17 03:05:34 -0500

    refactor to model tests

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -4,4 +4,21 @@ RSpec.describe BulkDiscount, type: :model do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe 'methods' do
+    it 'can find a discount by merchant id' do
+      merchant = Merchant.create(name: "Bob's Burger")
+      item_1 = merchant.items.create(name: 'Burger', description: 'its on a string', unit_price: 1000)
+      item_2 = merchant.items.create(name: 'Shake', description: 'dried grape', unit_price: 100)
+      customer = Customer.create(first_name: 'Teddy', last_name: 'Lastname')
+      invoice_1 = customer.invoices.create(status: 2)
+      invoice_2 = customer.invoices.create(status: 2)
+      invoice_item_1 = invoice_1.invoice_items.create!(item_id: item_1.id, quantity: 50, unit_price: 1, status: 1)
+      invoice_item_2 = invoice_1.invoice_items.create!(item_id: item_2.id, quantity: 50, unit_price: 2, status: 1)
+      discount1 = merchant.bulk_discounts.create!(discount: 10.00, quantity_threshold: 10)
+      discount2 = merchant.bulk_discounts.create!(discount: 50.00, quantity_threshold: 20)
+
+      expect(BulkDiscount.find_by_merchant_id(merchant.id)).to eq(discount1)
+    end
+  end
 end
